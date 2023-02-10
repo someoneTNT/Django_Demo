c7af143dab16def8be26af19d21ff81e49394897
someoneTNT <somebodyTNT@outlook.com>
2023-02-10 10:52:57 +0800

    测试

---
+++ Demo/Django_demo.py
@@ -0,0 +1,7 @@
+# author: dzj
+# date: 2023-02-10
+# desc: Django 学习
+
+#设置Anaconda镜像为清华
+#conda config --add channels - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free
+#conda config --add channels - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
